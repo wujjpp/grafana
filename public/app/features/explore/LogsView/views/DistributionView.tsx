76bd7d2b4f0505d17976041dd75db8356d85854d
Wu Jian Ping <wujp@greatld.com>
2021-12-21 12:06:45 +0800

    聚合组件分页增加设置分页大小功能

--- public/app/features/explore/LogsView/views/DistributionView.tsx
+++ public/app/features/explore/LogsView/views/DistributionView.tsx
@@ -4,12 +4,17 @@
 
 import React from 'react';
 import _ from 'lodash';
-import { stylesFactory, Pagination } from '@grafana/ui';
+import { stylesFactory, Pagination, Select, Label } from '@grafana/ui';
 import { css } from 'emotion';
 import { AbsoluteTimeRange } from '@grafana/data';
 import utils from '../utils';
 
-const PAGE_SIZE = 20;
+const pageSizeOptions = [
+  { value: 10, label: '10' },
+  { value: 20, label: '20' },
+  { value: 50, label: '50' },
+  { value: 100, label: '100' },
+];
 
 const styles = stylesFactory(() => {
   return {
@@ -73,6 +78,16 @@ const styles = stylesFactory(() => {
       overflow: hidden;
       background: rgb(50, 116, 217);
     `,
+
+    pagerContainer: css`
+      display: flex;
+      align-items: flex-end;
+    `,
+
+    pagerLabelContainer: css`
+      margin-right: 8px;
+      padding-bottom: 4px;
+    `,
   };
 })();
 
@@ -84,6 +99,7 @@ interface Props {
 }
 
 interface State {
+  pageSize: number;
   currentPage: number;
   currentList: Array<{ label: string; count: number; percent: number }>;
   list: Array<{ label: string; count: number; percent: number }>;
@@ -94,6 +110,7 @@ interface State {
 class DistributionView extends React.Component<Props, State> {
   state: State = {
     list: [],
+    pageSize: 20,
     currentPage: 1,
     currentList: [],
     totalRecords: 0,
@@ -122,9 +139,10 @@ class DistributionView extends React.Component<Props, State> {
         })
         .orderBy(['count'], ['desc'])
         .value();
-      const currentList = list.slice(0, PAGE_SIZE);
+      const currentList = list.slice(0, this.state.pageSize);
 
       this.setState({
+        ...this.state,
         list,
         currentList,
         totalRecords,
@@ -134,16 +152,22 @@ class DistributionView extends React.Component<Props, State> {
   }
 
   changeCurrentPage = (num: number) => {
-    const { list } = this.state;
-    const currentList = list.slice((num - 1) * PAGE_SIZE, num * PAGE_SIZE);
+    const { list, pageSize } = this.state;
+    const currentList = list.slice((num - 1) * pageSize, num * pageSize);
 
-    this.setState({ currentList, currentPage: num });
+    this.setState({ ...this.state, currentList, currentPage: num });
+  };
+
+  changePageSize = (pageSize: number) => {
+    const { list } = this.state;
+    const currentList = list.slice(0, pageSize);
+    this.setState({ ...this.state, pageSize, currentList, currentPage: 1 });
   };
 
   render() {
     const { fieldName, dataSourceInstanceName, absoluteTimeRange } = this.props;
-    const { list, currentPage, currentList, totalRecords, notNullCount } = this.state;
-    const totalPage = Math.ceil(list.length / PAGE_SIZE);
+    const { list, currentPage, currentList, totalRecords, notNullCount, pageSize } = this.state;
+    const totalPage = Math.ceil(list.length / pageSize);
 
     return (
       <div className={styles.container}>
@@ -178,12 +202,26 @@ class DistributionView extends React.Component<Props, State> {
               )}
             </div>
           ))}
+          {/* 这边直接用div来控制上下位置，只能这么干 */}
           {totalPage > 1 && (
-            <Pagination
-              currentPage={currentPage}
-              numberOfPages={totalPage}
-              onNavigate={this.changeCurrentPage}
-            ></Pagination>
+            <div className={styles.pagerContainer}>
+              <div className={styles.pagerLabelContainer}>
+                <Label>Items per page</Label>
+              </div>
+              <div>
+                <Select
+                  autoFocus={false}
+                  options={pageSizeOptions}
+                  value={pageSize}
+                  onChange={(v: any) => {
+                    this.changePageSize(v.value);
+                  }}
+                />
+              </div>
+              <div>
+                <Pagination currentPage={currentPage} numberOfPages={totalPage} onNavigate={this.changeCurrentPage} />
+              </div>
+            </div>
           )}
         </div>
       </div>
